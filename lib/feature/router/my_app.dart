17b42875bd54c676c5c9ef7380f9f0ffaa2f23da
PedRaM <p.alipour@karafarinbank.ir>
2024-01-01 15:11:12 +0330

    make app draggable

--- lib/feature/router/my_app.dart
+++ lib/feature/router/my_app.dart
@@ -1,3 +1,4 @@
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -59,7 +60,7 @@ class MyApp extends StatelessWidget {
                 textScaler: TextScaler.linear(scaleFactor),
                 boldText: false,
               ),
-              child: child!,
+              child: MoveWindow(child: child!),
             ),
           );
         },
